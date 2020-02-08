5c78bfcd88d74b5fc269a827b1ffbe8f53f81e0e
JMJ Riyana <jeemariyana@gmail.com>
2020-02-08 11:13:30 +0530

    getting input

--- hashcode.c
+++ hashcode.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main(){
-
+    long int M ,N;
+    int a[1000000000]
+    scanf("%ld %ld",&M,&N);
     
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+
+
     return 0;
 }
